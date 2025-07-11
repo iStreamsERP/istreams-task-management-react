6beabe252b5dc85d9451a19f6578bf58171d04f1
snehadharshini13 <snehadharshini@istreams-erp.com>
2025-07-11 13:40:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+ 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/public": {
-        target: "https://apps.istreams-erp.com:4439/iStreamsSmartService.asmx",
+        target: "https://apps.istreams-erp.com:4439/iStreamsSmartPublic.asmx",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/public/, ""),
